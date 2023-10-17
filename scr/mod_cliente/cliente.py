e19d3935eb763a6a68da03d9d7278d2d53d849dd
camil <camilacandidov@gmail.com>
2023-10-17 20:50:40 -0300

    listagem produto

--- scr/mod_cliente/cliente.py
+++ scr/mod_cliente/cliente.py
@@ -1,9 +1,20 @@
+import requests
+from settings import HEADERS_API, ENDPOINT_CLIENTE
 from flask import Blueprint, render_template
 bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
 
 @bp_cliente.route('/', methods=['GET', 'POST'])
 def formListaCliente():
-    return render_template('formListaCliente.html')
+    try:
+        response = requests.get(ENDPOINT_CLIENTE, headers=HEADERS_API)
+        result = response.json()
+        
+        if(response.status_code != 200):
+            raise Exception(result[0])
+        
+        return render_template('formListaCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
 
 @bp_cliente.route('/form-cliente/', methods=['GET'])
 def formCliente():
