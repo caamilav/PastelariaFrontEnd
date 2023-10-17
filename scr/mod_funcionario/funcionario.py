4dfcb8ebb642d574a25543b02c2a904fad063cd3
camil <camilacandidov@gmail.com>
2023-10-17 20:29:56 -0300

    ajustes

--- scr/mod_funcionario/funcionario.py
+++ scr/mod_funcionario/funcionario.py
@@ -1,10 +1,21 @@
+import requests
+from settings import HEADERS_API, ENDPOINT_FUNCIONARIO
 from flask import Blueprint, render_template
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
 
 @bp_funcionario.route('/', methods=['GET', 'POST'])
 def formListaFuncionario():
-    return render_template('formListaFuncionario.html')
+    try: 
+        response = requests.get(ENDPOINT_FUNCIONARIO, headers=HEADERS_API)
+        result = response.json()
+        
+        if (response.status_code != 200):
+          raise Exception(result[0])
+        
+        return render_template('formListaFuncionario.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
 
 @bp_funcionario.route('/form-funcionario/', methods=['GET'])
 def formFuncionario():
--- scr/settings.py
+++ scr/settings.py
@@ -11,3 +11,13 @@ load_dotenv(dotenv_file)
 HOST  = os.getenv("HOST")
 PORT  = os.getenv("PORT")
 DEBUG = os.getenv("DEBUG")
+
+# Configurações da API
+URL_API = os.getenv("URL_API")
+HEADERS_API = {'x-token': os.getenv("X_TOKEN"), 'x-key': os.getenv("X_KEY")}
+
+# Configuração dos endpoints
+ENDPOINT_LOGIN = os.getenv("ENDPOINT_LOGIN")
+ENDPOINT_FUNCIONARIO = os.getenv("ENDPOINT_FUNCIONARIO")
+ENDPOINT_CLIENTE = os.getenv("ENDPOINT_CLIENTE")
+ENDPOINT_PRODUTO = os.getenv("ENDPOINT_PRODUTO")
