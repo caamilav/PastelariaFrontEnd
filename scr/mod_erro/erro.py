140cd0c6fa7b8eb4a31c12e3774b67ff0174ecfd
camil <camilacandidov@gmail.com>
2023-11-20 00:11:54 -0300

    Ajustes no front da pagina de login e pagina de erro

--- scr/mod_erro/erro.py
+++ scr/mod_erro/erro.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
-bp_erro = Blueprint('erro', __name__,
-template_folder='templates')
+from mod_login.login import validaSessao
+bp_erro = Blueprint('erro', __name__, template_folder='templates')
 
 @bp_erro.app_errorhandler(404)
+@validaSessao
 def erro404(error):
     return render_template("form404.html", erroHttp=error)
 
 @bp_erro.app_errorhandler(500)
+@validaSessao
 def erro500(error):
     return render_template("form500.html", erroHttp=error)
